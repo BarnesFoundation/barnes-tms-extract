53261a35ffa63e36040e4d5e5849ddd55b3bdbb8
Sam Tarakajian <starakajian@gmail.com>
2017-02-14 13:04:37 -0500

    Added a little bit of JSON parsing to the ArtObject

--- tms_csv/src/artObject.js
+++ tms_csv/src/artObject.js
@@ -1,6 +1,16 @@
 export default class ArtObject {
-	constructor(jsonDescription) {
-		this._description = jsonDescription;
+	constructor(jsonDescription, collection="Common") {
+		const jsonObj = JSON.parse(jsonDescription);
+
+		this._artist = jsonObj.people.value;
+		this._title = jsonObj.title.value;
+		this._displayDate = jsonObj.displayDate !== undefined ? jsonObj.displayDate.value : undefined;
+		this._medium = jsonObj.medium.value;
+		this._dimensions = jsonObj.dimensions.value;
+		this._accessionNumber = jsonObj.invno.value;
+		this._description = jsonObj.description.value;
+		this._primaryMedia = jsonObj.primaryMedia.value;
+		this._id = jsonObj.id.value;
 	}
 
 	get description() {
