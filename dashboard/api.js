746ae831f8da3d82191ebab1707bebd718bd40cf
Cassie Tarakajian <ctarakajian@gmail.com>
2017-02-24 16:01:38 -0500

    add scaffold for tmstocsv microservice

--- dashboard/api.js
+++ dashboard/api.js
@@ -3,9 +3,17 @@ function api(options) {
   //an example of configuring a path to a microservice
   // var valid_ops = { sum:'sum', product:'product' }
 
-  this.add( 'role:api,path:test', function( msg, respond ) {
+  var valid_commands = {info:'info'};
+  this.add('role:api,path:test', function( msg, respond ) {
     this.act( 'role:test', {}, respond )
-  })
+  });
+
+  this.add('role:api,path:tmstocsv', function(msg, respond) {
+    //do something to msg if necessary
+    //msg.args.params url params, msg.args.query query string params
+    var command = msg.args.params.cmd
+    this.act('role:tmstocsv', {cmd: valid_commands[command]}, respond);
+  });
 
   this.add( 'init:api', function( msg, respond ) {
     this.act('role:web',{routes:{
@@ -13,7 +21,8 @@ function api(options) {
       pin:    'role:api,path:*',
       //this is where we add the route to the microservice
       map: {
-        test: { GET:true }
+        test: { GET:true },
+        tmstocsv: { GET:true, suffix:'/:cmd' }
       }
     }}, respond )
   });
--- dashboard/index.js
+++ dashboard/index.js
@@ -4,6 +4,7 @@ var Express = require('express');
 var Router = Express.Router;
 var context = new Router();
 var path = require('path');
+var shelljs = require('shelljs');
 
 var senecaWebConfig = {
       context: context,
@@ -20,8 +21,17 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/last-run', function(req, res) {
+  var runTimesString = shelljs.grep("Beginning CSV export", "../tms_csv/logs/all-logs.log");
+  var lines = runTimesString.split('\n');
+  lines.pop();
+  var lastRunTime = JSON.parse(lines[lines.length-1]);
+  res.json({time: lastRunTime.timestamp});
+});
+
 var seneca = require( 'seneca' )()
       .use( SenecaWeb, senecaWebConfig )
       .use( 'api' )
 //this is where we list the microservices
       .client( { type:'tcp', pin:'role:test' } )
+      .client( { type:'tcp', pin:'role:tmstocsv' } )
