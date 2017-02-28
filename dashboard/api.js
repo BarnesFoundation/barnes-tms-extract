a627141296a1424957edf243af703eedfdc0004f
Cassie Tarakajian <ctarakajian@gmail.com>
2017-02-28 18:15:20 -0500

    run tmscsv script from web page

--- dashboard/api.js
+++ dashboard/api.js
@@ -3,7 +3,7 @@ function api(options) {
   //an example of configuring a path to a microservice
   // var valid_ops = { sum:'sum', product:'product' }
 
-  var valid_commands = {info:'info'};
+  var valid_commands = {info:'info', run:'run'};
   this.add('role:api,path:test', function( msg, respond ) {
     this.act( 'role:test', {}, respond )
   });
