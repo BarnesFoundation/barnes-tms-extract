4d074f8be8877f243ee6ca44265cab29274ca2f5
Cassie Tarakajian <ctarakajian@gmail.com>
2017-02-28 18:15:20 -0500

    run tmscsv script from web page

--- dashboard/tmstocsv.js
+++ dashboard/tmstocsv.js
@@ -1,6 +1,23 @@
+var shelljs = require('shelljs');
+var exec = require('child_process').exec;
+
+function lastRunTime() {
+  var runTimesString = shelljs.grep("Beginning CSV export", "../tms_csv/logs/all-logs.log");
+  var lines = runTimesString.split('\n');
+  lines.pop();
+  var lastRunTime = JSON.parse(lines[lines.length-1]);
+  return lastRunTime.timestamp;
+}
+
+
 function tmstocsv(options) {
   this.add('role:tmstocsv,cmd:info', function info(msg, respond) {
-    respond( null, {message: 'testing'});
+    respond( null, {time: lastRunTime()});
+  });
+
+  this.add('role:tmstocsv,cmd:run', function run(msg, respond) {
+    exec("cd ../tms_csv && npm start &");
+    respond(null, {time: lastRunTime()});
   });
 }
 
