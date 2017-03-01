7f22d7f5fb95030a607bc3b5f7c225bbee948076
Cassie Tarakajian <ctarakajian@gmail.com>
2017-03-01 14:39:44 -0500

    remove test service

--- dashboard/test-pin-service.js
+++
@@ -1,7 +0,0 @@
-require('seneca')()
-
-  .use('test')
-
-  // listen for role:math messages
-  // IMPORTANT: must match client
-  .listen({ type: 'tcp', pin: 'role:test' });
--- dashboard/test.js
+++
@@ -1,7 +0,0 @@
-function test(options) {
-	this.add('role:test', (msg, respond) => {
-		respond(null, { message: 'test is successful!' });
-	});
-}
-
-module.exports = test;
