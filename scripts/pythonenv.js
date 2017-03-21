e40428db16e5f9c819b40cd9382a0eb4f0d9bf1d
Cassie Tarakajian <ctarakajian@gmail.com>
2017-03-21 15:04:24 -0400

    add path to bash in postinstall

--- scripts/pythonenv.js
+++ scripts/pythonenv.js
@@ -1,11 +1,14 @@
 var shell = require('shelljs');
 
+var output = shell.exec("which bash");
+var bashPath = output.stdout;
+
 shell.echo("Creating tmsdiff Python environment");
-shell.exec("conda create -n tmsdiff -yq");
+shell.exec("conda create -n tmsdiff -yq", { shell: bashPath });
 
 shell.echo("Installing python dependencies");
-shell.exec("source activate tmsdiff");
-shell.exec("pip install -r py_csv_diff/requirements.txt");
+shell.exec("source activate tmsdiff", { shell: bashPath });
+shell.exec("pip install -r py_csv_diff/requirements.txt", { shell: bashPath });
 
 shell.echo("Deactivating python source");
-shell.exec("source deactivate");
+shell.exec("source deactivate", { shell: bashPath });
