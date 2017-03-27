0bc5edf05407dc5fde0594058c455216a33d3fdc
Sam Tarakajian <starakajian@gmail.com>
2017-03-27 12:20:57 -0400

    added 'latest' csv name for csv_es/src/index

--- csv_es/src/script/index.js
+++ csv_es/src/script/index.js
@@ -1,5 +1,6 @@
 const ESCollection = require('./esCollection.js');
 const logger = require('./esLogger.js');
+const { getLastCompletedCSV } = require('./csvUtil.js');
 
 const esHost = 'localhost:9200';
 
@@ -9,7 +10,11 @@ const esCollection = new ESCollection(esHost);
 
 const csvRootDir = argv._[0];
 
-const targetCSV = argv._[1];
+let targetCSV = argv._[1];
+
+if (targetCSV === 'latest') {
+	targetCSV = getLastCompletedCSV(csvRootDir);
+}
 
 esCollection.init().then(() => return esCollection.clearCollectionObjects).then(() => {
 	return esCollection.syncESToCSV(path.join(csvRootDir, targetCSV, "objects.csv"));
