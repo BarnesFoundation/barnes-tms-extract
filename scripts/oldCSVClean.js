ab56a4b9344f5b1f13fc7ed56e99cb9a98d0910e
Sam Tarakajian <starakajian@gmail.com>
2017-03-29 10:19:59 -0400

    Added script to clean up old CSVs

---
+++ scripts/oldCSVClean.js
@@ -0,0 +1,32 @@
+const fs = require('fs-extra');
+const moment = require('moment');
+const path = require('path');
+
+const argv = require('minimist')(process.argv.slice(2));
+
+const source = argv.d || path.resolve(__dirname, '../dashboard/public/output');
+
+const cutoff = moment().subtract(15, 'days');
+
+// Remove every directory older than 15 days
+const dirs = fs.readdirSync(source);
+
+const dirsToDelete = [];
+
+dirs.forEach((d) => {
+	if (d.startsWith('csv_')) {
+		const ts = parseInt(d.split('_')[1]);
+
+		const csvTime = moment(ts);
+
+		if (csvTime.isBefore(cutoff)) {
+			dirsToDelete.push(d);
+		}
+	}
+});
+
+dirsToDelete.forEach((d) => {
+	const dpath = path.join(source, d);
+
+	fs.removeSync(dpath);
+});
