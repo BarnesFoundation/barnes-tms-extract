06887f7ada8ce31f6745c4fcb2516017829f587f
Sam Tarakajian <starakajian@gmail.com>
2017-04-04 11:09:15 -0400

    Added CSV polling to update the CSV table automatically while the server is running

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -22,30 +22,42 @@ app.set('views', path.resolve(`${__dirname}/../views`));
 app.listen(3000);
 
 const seneca = require('seneca')()
-      .use(SenecaWeb, senecaWebConfig)
-      .use('api')
+			.use(SenecaWeb, senecaWebConfig)
+			.use('api')
 // this is where we list the microservices
-      .client({ type: 'tcp', pin: 'role:tmstocsv' })
-      .client({ type: 'tcp', pin: 'role:csv', port: 10202 })
-      .client({ type: 'tcp', pin: 'role:es', port: 10203 })
-      .client({ type: 'tcp', pin: 'role:images', port: 10204 });
+			.client({ type: 'tcp', pin: 'role:tmstocsv' })
+			.client({ type: 'tcp', pin: 'role:csv', port: 10202 })
+			.client({ type: 'tcp', pin: 'role:es', port: 10203 })
+			.client({ type: 'tcp', pin: 'role:images', port: 10204 });
 
 app.get('/', (req, res) => {
 	let desc;
 	let info;
 	let list;
-  let imageInfo;
+	let imageInfo;
 	seneca.act('role:es,cmd:desc', (err, result) => {
 		desc = result;
 		seneca.act('role:tmstocsv,cmd:info', (err, result) => {
 			info = result;
-      seneca.act('role:images,cmd:info', (err, result) => {
-        imageInfo = result;
-  			seneca.act('role:csv,cmd:list', (err, result) => {
-  				list = result;
-  				res.render('index', { desc, info, list, imageInfo, moment });
-  			});
-      });
+			seneca.act('role:images,cmd:info', (err, result) => {
+				imageInfo = result;
+				seneca.act('role:csv,cmd:list', (err, result) => {
+					list = result;
+					res.render('index', { desc, info, list, imageInfo, moment });
+				});
+			});
+		});
+	});
+});
+
+app.get('/csvFiles', (req, res) => {
+	let desc;
+	let list;
+	seneca.act('role:es,cmd:desc', (err, result) => {
+		desc = result;
+		seneca.act('role:csv,cmd:list', (err, result) => {
+			list = result;
+			res.render('csvFiles', { list, desc });
 		});
 	});
 });
