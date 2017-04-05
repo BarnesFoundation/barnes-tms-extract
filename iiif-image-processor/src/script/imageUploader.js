914b655d88c11bef97fdc60fa95e61cbe2d90dcb
Cassie Tarakajian <ctarakajian@gmail.com>
2017-04-05 12:59:44 -0400

    add script to upload raw images, update architecture image

--- iiif-image-processor/src/script/imageUploader.js
+++ iiif-image-processor/src/script/imageUploader.js
@@ -6,8 +6,11 @@ const credentials = require('../../credentials.json');
 const { exec, execSync }  = require('child_process');
 const path = require('path');
 const s3 = require('s3');
+const AWS = require('aws-sdk');
 const csv = require('fast-csv');
 const fs = require('fs');
+const https = require('https');
+const eachSeries = require('async/eachSeries');
 
 class ImageUploader extends UpdateEmitter {
 	constructor(csvDir) {
@@ -16,15 +19,20 @@ class ImageUploader extends UpdateEmitter {
 		this._isRunning = false;
 		this._availableImages = null;
 		this._tiledImages = null;
+		this._rawImages = null;
 		this._numImagesProcessed = 0;
 		this._numImagesToProcess = 0;
+		this._currentStep = "Not started.";
 		logger.info('creating image logger');
+		const cred = new AWS. SharedIniFileCredentials({
+		  profile: "barnes"
+		});
+		const awss3 = new AWS.S3({
+		  credentials: cred,
+		  region: credentials.awsRegion
+		});
 		this._s3Client = s3.createClient({
-			s3Options: {
-				accessKeyId: credentials.awsAccessKeyId,
-				secretAccessKey: credentials.awsSecretAccessKey,
-				region: credentials.awsRegion
-			}
+			s3Client: awss3
 		});
 		this._fetchTiledImages();
 	}
@@ -37,6 +45,7 @@ class ImageUploader extends UpdateEmitter {
 			isRunning: this._isRunning,
 			currentStep: this._currentStep,
 			numTiledImages: this._tiledImages ? this._tiledImages.length : 0,
+			numRawImages: this._rawImages ? this._rawImages.length : 0,
 		};
 	}
 
@@ -46,23 +55,38 @@ class ImageUploader extends UpdateEmitter {
 		logger.info('Starting process to tile images.');
 		const imageDirPromise = this._fetchAvailableImages();
 		this._currentStep = "Fetching available images from TMS.";
+		this.progress();
 		imageDirPromise.then(() => {
 			logger.info('Pulled list of available images from TMS successfully.');
+			this._currentStep = "Fetching list of images uploaded to S3.";
+			this.progress();
 			this._fetchTiledImages().then(() => {
-				const lastCSV = getLastCompletedCSV(this._csvDir);
-				const csvPath = path.join(this._csvDir, lastCSV, 'objects.csv');
-				const imagesToProcess = [];
-				csvForEach(csvPath, (row) => {
-					const img = this._imageNeedsUpload(`${row.invno}.jpg`);
-					if (img) {
-						imagesToProcess.push(img);
-					}
-				},
-				() => {
-					this._tileAndUpload(imagesToProcess);
-					this._updateTiledList(imagesToProcess).then(() => {
-						this.completed();
-						this._isRunning = false;
+				this._fetchRawImages().then(() => {
+					this._currentStep = "Determining which images need to be uploaded or tiled.";
+					this.progress();
+					const lastCSV = getLastCompletedCSV(this._csvDir);
+					const csvPath = path.join(this._csvDir, lastCSV, 'objects.csv');
+					const imagesToProcess = [];
+					const imagesToUpload = [];
+					csvForEach(csvPath, (row) => {
+						const img = this._imageNeedsUpload(`${row.invno}.jpg`);
+						if (img) {
+							imagesToProcess.push(img);
+						}
+						const rawImg = this._rawImageNeedsUpload(`${row.invno}.tif`);
+						if (rawImg) {
+							imagesToUpload.push(rawImg);
+						}
+					},
+					() => {
+						this._tileAndUpload(imagesToProcess);
+						this._updateTiledList(imagesToProcess);
+						this._uploadRaw(imagesToUpload);
+						this._updateRawList(imagesToUpload).then(() => {
+							this._currentStep = "Finished.";
+							this.completed();
+							this._isRunning = false;
+						});
 					});
 				});
 			});
@@ -95,12 +119,39 @@ class ImageUploader extends UpdateEmitter {
 		});
 	}
 
+	_updateRawList(images) {
+		const csvStream = csv.createWriteStream({headers: true});
+    const writableStream = fs.createWriteStream(path.resolve(__dirname, '../../raw.csv'));
+
+    return new Promise((resolve) => {
+	    writableStream.on("finish", () => {
+		  	this._s3Client.uploadFile({
+		  		localFile: path.resolve(__dirname, '../../raw.csv'),
+		  		s3Params: {
+						Bucket: credentials.awsBucket,
+						Key: 'raw.csv'
+					}
+		  	})
+		  	.on('end', () => {
+		  		resolve();
+		  	});
+		  });
+
+			csvStream.pipe(writableStream);
+			this._rawImages.concat(images).forEach((img) => {
+				csvStream.write(img);
+			});
+			csvStream.end();
+		});
+	}
+
 	_fetchAvailableImages() {
 		logger.info('Starting fetch available images.');
 		const getImageUrlPath = path.resolve(__dirname, './getImageUrls.js');
 		const outputPath = path.resolve(__dirname, '../../names.json');
 		const url = credentials.barnesImagesUrl;
-		const cmd = `phantomjs --ignore-ssl-errors=true --ssl-protocol=tlsv1 --web-security=false ${getImageUrlPath} ${url} ${outputPath}`;
+		// const cmd = `phantomjs --ignore-ssl-errors=true --ssl-protocol=tlsv1 --web-security=false ${getImageUrlPath} ${url} ${outputPath}`;
+		const cmd = 'echo hello';
 		return new Promise((resolve) => {
 			const phantom = exec(cmd, () => {
 				this._isInitialized = true;
@@ -141,8 +192,37 @@ class ImageUploader extends UpdateEmitter {
 		});
 	}
 
+	_fetchRawImages() {
+		logger.info('Starting to fetch raw images uploaded.');
+		this._rawImages = [];
+		return new Promise((resolve) => {
+			this._s3Client.downloadFile({
+				s3Params: {
+					Bucket: credentials.awsBucket,
+					Key: 'raw.csv'
+				},
+				localFile: path.resolve(__dirname, '../../raw.csv')
+			})
+			.on('error', (err) => {
+				if (err.message === "http status code 404") {
+					logger.info('Can\'t fetch list of raw images--hasn\'t been created yet.');
+					this._rawImages = [];
+					resolve();
+				}
+			})
+			.on('end', () => {
+				logger.info('raw.csv has been downloaded--loading into memory.');
+				csvForEach(path.resolve(__dirname, '../../raw.csv'), (data) => {
+					this._rawImages.push({name: data.name, size: data.size, modified: data.modified});
+				}, () => {
+					resolve();
+				});
+			})
+		});
+	}
+
 	_imageNeedsUpload(imgName) {
-		logger.info(`Checking if image ${imgName} needs upload`);
+		logger.info(`Checking if image ${imgName} needs upload.`);
 		const s3Found = this._tiledImages.find((element) => element.name.toLowerCase() === imgName.toLowerCase());
 		const tmsFound = this._availableImages.find((element) => element.name.toLowerCase() === imgName.toLowerCase());
 
@@ -170,6 +250,8 @@ class ImageUploader extends UpdateEmitter {
 		this._numImagesToProcess = images.length;
 		this.progress();
 		images.forEach((image, index) => {
+			this._currentStep = `Tiling image: ${image.name}`;
+			this.progress();
 			logger.info(`Tiling image: ${image.name}, ${index + 1} of ${this._numImagesToProcess}`);
 			const cmd = `./go-iiif/bin/iiif-tile-seed -config config.json -endpoint http://barnes-image-repository.s3-website-us-east-1.amazonaws.com/tiles -verbose -loglevel debug ${image.name}`;
 			execSync(cmd, (error, stdout, stderr) => {
@@ -182,6 +264,66 @@ class ImageUploader extends UpdateEmitter {
 			});
 			this._numImagesProcessed = index + 1;
 			this.progress();
+		});
+	}
+
+	_rawImageNeedsUpload(imgName) {
+		logger.info(`Checking if raw image ${imgName} needs upload.`);
+		const s3Found = this._rawImages.find((element) => element.name.toLowerCase() === imgName.toLowerCase());
+		const tmsFound = this._availableImages.find((element) => element.name.toLowerCase() === imgName.toLowerCase());
+
+		// if the picture is on TMS
+		// 		if the picture is on S3 but has changed
+		//    or if the picture is not on S3
+		//		then it needs upload
+		// else it does not
+		if (tmsFound) {
+			if (s3Found && (s3Found.size !== tmsFound.size || s3Found.modified !== tmsFound.modified)) {
+				logger.info(`${imgName} is available on TMS, has already been uploaded, but has changed.`);
+				return tmsFound;
+			} else if (!s3Found) {
+				logger.info(`${imgName} is available on TMS, but never has been uploaded.`);
+				return tmsFound;
+			}
+			logger.info(`${imgName} is available on TMS, has been uploaded, and has not changed.`);
+			return false;
+		}
+		logger.info(`${imgName} is not available on TMS.`);
+		return false;
+	}
+
+	_uploadRaw(images) {
+		this._numImagesToProcess = images.length;
+		let index = 1;
+		eachSeries(images, (image, cb) => {
+			this._currentStep = `Uploading raw image: ${image.name}`;
+			this._numImagesProcessed = index;
+			this.progress();
+			const file = fs.createWriteStream(path.resolve(__dirname, `./${image.name}`));
+			https.get(`${credentials.barnesImagesUrl}${image.name}`, (response) => {
+			  response.pipe(file);
+			  file.on('finish', () => {
+
+					logger.info(`Uploading raw image ${image.name}.`);
+					this._s3Client.uploadFile({
+						s3Params: {
+							Bucket: credentials.awsBucket,
+							Key: `raw/${image.name}`
+						},
+						localFile: path.resolve(__dirname, `./${image.name}`)
+					})
+					.on('err', (err) => {
+						cb(err);
+					})
+					.on('end', () => {
+						index += 1;
+						fs.unlink(path.resolve(__dirname, `./${image.name}`), cb);
+					})
+			  });
+			});
+		}, (err) => {
+			logger.info('Finished uploading all raw images.');
+			if (err) logger.error(err);
 		});
 	}
 
