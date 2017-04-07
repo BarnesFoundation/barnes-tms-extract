d1cacd45d62cbd9289aba2a9957fa7b26ec154f5
Sam Tarakajian <starakajian@gmail.com>
2017-04-07 11:36:08 -0400

    Continuing the big directory refactor

--- es_import/src/scripts/pythonenv.js
+++ es_import/src/scripts/pythonenv.js
@@ -1,14 +1,16 @@
-var shell = require('shelljs');
+const path = require('path');
+const shell = require('shelljs');
 
-var output = shell.exec("which bash");
-var bashPath = output.stdout.trim();
+const output = shell.exec("which bash");
+const bashPath = output.stdout.trim();
+const reqPath = path.resolve(__dirname, '../py_csv_diff/requirements.txt');
 
 shell.echo("Creating tmsdiff Python environment");
 shell.exec("conda create -n tmsdiff -yq", { shell: bashPath });
 
 shell.echo("Installing python dependencies");
 shell.exec("source activate tmsdiff", { shell: bashPath });
-shell.exec("pip install -r py_csv_diff/requirements.txt", { shell: bashPath });
+shell.exec(`pip install -r ${reqPath}`, { shell: bashPath });
 
 shell.echo("Deactivating python source");
 shell.exec("source deactivate", { shell: bashPath });
