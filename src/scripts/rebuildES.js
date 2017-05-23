50547a67a894638714b1c49e89cb4ee87bfab908
Sam Tarakajian <starakajian@gmail.com>
2017-05-23 13:06:58 -0400

    Adding file to rebuild the ES index from an incompatible mapping.json

---
+++ src/scripts/rebuildES.js
@@ -0,0 +1,22 @@
+const config = require('config');
+const { makeElasticsearchOptions } = require('../util/elasticOptions.js');
+const ESCollection = require('../csv_es/src/script/esCollection.js');
+
+const esCollection = new ESCollection( makeElasticsearchOptions(), config.CSV.path);
+let rebuildCSVTarget = null;
+esCollection.description.then((desc) => {
+	if (desc.lastImportedCSV !== null) {
+		rebuildCSVTarget = desc.lastImportedCSV;
+		return esCollection._deleteCollectionIndex();
+	} else {
+		throw {message: "ES is unsynchronized and cannot be rebuilt"}
+	}
+}).then(() => {
+	return esCollection._createCollectionIndex();
+}).then(() => {
+	return esCollection.syncESToCSV(rebuildCSVTarget);
+}).then(() => {
+	return esCollection.description();
+}).then((desc) => {
+	console.log(desc);
+});
