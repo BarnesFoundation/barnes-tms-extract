6cef425e8e028c03595900b41c3ad17f27a68e34
Cassie Tarakajian <ctarakajian@gmail.com>
2017-06-15 13:29:46 -0400

    remove logging, change .includes to .startsWith

--- src/image-processing/src/script/imageResizer.js
+++ src/image-processing/src/script/imageResizer.js
@@ -61,11 +61,8 @@ class ImageResizer extends UpdateEmitter {
 				logger.info(`Checking if ${row.invno}.jpg needs to be resized.`);
 				const availableImage = this._availableImages.find((image) => image.key === `${row.invno}.jpg`);
 				if (!availableImage) return;
-				const resizedImage = this._resizedImages.find((image) => {
-					console.log(image.key);
-					return image.key.includes(row.id) && !image.key.includes('_o')
-				});
-				const originalImage = this._resizedImages.find((image) => image.key.includes(row.id) && image.key.includes('_o'));
+				const resizedImage = this._resizedImages.find((image) => {image.key.startsWith(row.id) && !image.key.includes('_o'));
+				const originalImage = this._resizedImages.find((image) => image.key.startsWith(row.id) && image.key.includes('_o'));
 				if (!resizedImage) {
 					imagesToResize.push(Object.assign({}, row, availableImage));
 					return;
