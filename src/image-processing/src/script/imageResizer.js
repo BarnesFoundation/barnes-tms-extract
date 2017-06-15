2d87acebfad259a15f53b8069c6347ced957422e
Cassie Tarakajian <ctarakajian@gmail.com>
2017-06-15 13:27:01 -0400

    more debugging

--- src/image-processing/src/script/imageResizer.js
+++ src/image-processing/src/script/imageResizer.js
@@ -59,12 +59,12 @@ class ImageResizer extends UpdateEmitter {
 			this.progress();
 			csvForEach(csvPath, (row) => {
 				logger.info(`Checking if ${row.invno}.jpg needs to be resized.`);
-				const availableImage = this._availableImages.find((image) => {
-					console.log(image.key);
-					return image.key === `${row.invno}.jpg`
-				});
+				const availableImage = this._availableImages.find((image) => image.key === `${row.invno}.jpg`);
 				if (!availableImage) return;
-				const resizedImage = this._resizedImages.find((image) => image.key.includes(row.id) && !image.key.includes('_o'));
+				const resizedImage = this._resizedImages.find((image) => {
+					console.log(image.key);
+					return image.key.includes(row.id) && !image.key.includes('_o')
+				});
 				const originalImage = this._resizedImages.find((image) => image.key.includes(row.id) && image.key.includes('_o'));
 				if (!resizedImage) {
 					imagesToResize.push(Object.assign({}, row, availableImage));
