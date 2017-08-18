961a061608988e35ec6fe1287b78fe68d20c132a
rnackman <rachel.nackman@gmail.com>
2017-08-18 13:58:41 -0400

    Add debugging script to import specified collection script to ES. WIP.

---
+++ src/scripts/debugging/csvToES.js
@@ -0,0 +1,25 @@
+const csvTimestamp = 1502829223397;
+
+const config = require('config');
+const path = require('path');
+
+const ESCollection = require('../../csv_es/src/script/esCollection.js');
+const { makeElasticsearchOptions } = require('../../util/elasticOptions.js');
+
+const logger = require('../../csv_es/src/script/esLogger.js');
+
+const csvRootDir = 'src/dashboard/public/output/';
+const csv = csvRootDir+'csv_'+csvTimestamp+'/objects.csv';
+const csvDate = new Date(csvTimestamp);
+const csvPath = path.basename(path.dirname(csv));
+
+logger.info(csvPath);
+logger.info('Beginning import of', csvPath, '...');
+
+const options = makeElasticsearchOptions();
+
+const esCollection = new ESCollection(options, csvRootDir);
+
+esCollection.syncESToCSV(csvPath);
+
+// 'src/dashboard/public/output/csv_1502813474684'
