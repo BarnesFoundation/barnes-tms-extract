bcebda01d0cfb83c9c115692296a4146d7f130bb
rnackman <rachel.nackman@gmail.com>
2017-08-22 15:08:14 -0400

    Add option to pass --all flag and import all CSVs in data dir.

--- src/scripts/importCSV.js
+++ src/scripts/importCSV.js
@@ -1,28 +1,41 @@
 const config = require('config');
 const path = require('path');
-const logger = require('../util/logger.js')(path.join(__dirname, "logs/cron_logs.txt"));
-const { makeElasticsearchOptions } = require('../util/elasticOptions.js');
-const ESCollection = require('../csv_es/src/script/esCollection.js');
-
+const fs = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
 
-logger.info(argv.csv);
+const logger = require('../util/logger.js')(path.join(__dirname, "logs/cron_logs.txt"));
 
-let importCSVTarget = argv.csv || null;
-
+const { makeElasticsearchOptions } = require('../util/elasticOptions.js');
+const ESCollection = require('../csv_es/src/script/esCollection.js');
 const esCollection = new ESCollection( makeElasticsearchOptions(), config.CSV.path );
 
-esCollection.description().then((desc) => {
-  if ( importCSVTarget ) {
-    logger.info('Preparing index to import ', importCSVTarget);
-    return esCollection._prepareIndexForSync();
-  } else {
-    logger.error('You need to indicate a CSV for import.');
-    logger.error('Usage: node src/scripts/importCSV.js --csv=test.csv');
-    throw { message: "No CSV to import." };
-  }
-}).then(() => {
-  esCollection.importDataCSVToES(importCSVTarget);
-});
+if (argv.all) {
+  esCollection.description().then((desc) => {
+    fs.readdir(config.CSV.dataPath, (err, files) => {
+      const csvFiles = files.filter(function(file) {
+        return file.indexOf('.csv') !== -1;
+      });
+      csvFiles.forEach((csv) => {
+        esCollection.importDataCSVToES(csv);
+      });
+    })
+  });
+} else {
+  let importCSVTarget = argv.csv || null;
+  esCollection.description().then((desc) => {
+    if ( importCSVTarget ) {
+      logger.info('Preparing index to import ', importCSVTarget);
+      return esCollection._prepareIndexForSync();
+    } else {
+      logger.error('You need to indicate a CSV for import.');
+      logger.error('To import specific CSV: node src/scripts/importCSV.js --csv=test.csv');
+      logger.error('To import all CSVs in data dir: node src/scripts/importCSV.js --all');
+      throw { message: "No CSV to import." };
+    }
+  }).then(() => {
+    esCollection.importDataCSVToES(importCSVTarget);
+  });
+}
+
 
 
