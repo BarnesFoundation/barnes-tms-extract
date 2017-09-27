11b7419be876eac2259851d3f758f2d66b83c2c5
rnackman <rachel.nackman@gmail.com>
2017-09-27 16:57:42 -0400

    Clean up image secrets script.

--- src/scripts/updates/03_addImageSecretsToES.js
+++ src/scripts/updates/03_addImageSecretsToES.js
@@ -1,16 +1,9 @@
 const config = require('config');
 const path = require('path');
-const fs = require('fs');
-const csv = require('fast-csv');
-const eachSeries = require('async/eachSeries');
-
-const { csvForEach } = require('../../util/csvUtil.js');
 const ESCollection = require('../../csv_es/src/script/esCollection.js');
-
-const csvDir = config.CSV.imageSecretsPath;
 const { makeElasticsearchOptions } = require('../../util/elasticOptions.js');
-const esClient = new ESCollection(makeElasticsearchOptions(), csvDir);
 
+const esClient = new ESCollection(makeElasticsearchOptions(), config.CSV.imageSecretsPath);
 const csvPath = path.join(config.CSV.imageSecretsPath, config.CSV.imageSecretsFilename);
 
 esClient._updateESWithImageSecrets(csvPath);
