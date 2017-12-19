84de0a77a0f7d8a6135640d028c58467409eee42
Jonathan Dahan <hi@jonathan.is>
2017-12-19 14:34:07 -0500

    Rebuild entire collection on each import, with the current timestamp
    We removed a decent amount of csv partial import functions
    
    It will be nice to remove more

--- src/scripts/updates/00_prepareESIndex.js
+++ src/scripts/updates/00_prepareESIndex.js
@@ -1,13 +1,13 @@
-const config = require('config');
-const ESCollection = require('../../csv_es/src/script/esCollection.js');
-const { makeElasticsearchOptions } = require('../../util/elasticOptions.js');
+// okay all this uses is  makeElasticSearchOptions and prepareIndexForSync and description
 
-const options = makeElasticsearchOptions();
-const csvRootDir = 'src/dashboard/public/output/';
-const esCollection = new ESCollection(options, csvRootDir);
+const ESCollection = require('../../csv_es/src/script/esCollection.js')
+const { makeElasticsearchOptions } = require('../../util/elasticOptions.js')
+const logger = require('../../csv_es/src/script/esLogger.js')
 
-const logger = require('../../csv_es/src/script/esLogger.js');
+const options = makeElasticsearchOptions()
+const csvRootDir = 'src/dashboard/public/output/'
+const esCollection = new ESCollection(options, csvRootDir)
 
 esCollection._prepareIndexForSync().then((res) => {
-  esCollection.description().then((res) => { logger.info(res) });
+  esCollection.description().then((res) => { logger.info(res) })
 });
