89c48daa8d3ce1a50d1a81114381819bcaa04246
Jonathan Dahan <hi@jonathan.is>
2018-02-07 13:27:54 -0500

    kinda revert d0cfacbd49dcea6ac98c90e41a3551b578eeaa57 for imagesecrets

--- src/scripts/updates/03_addImageSecretsToES.js
+++ src/scripts/updates/03_addImageSecretsToES.js
@@ -3,9 +3,7 @@ const path = require('path');
 const ESCollection = require('../../csv_es/src/script/esCollection.js');
 const { makeElasticsearchOptions } = require('../../util/elasticOptions.js');
 
-const esClient = new ESCollection(makeElasticsearchOptions(), config.CSV.dataPath);
-const csvPath = path.join(config.CSV.dataPath, config.CSV.imageSecretsFilename);
+const esClient = new ESCollection(makeElasticsearchOptions(), config.CSV.imageSecretsPath);
+const csvPath = path.join(config.CSV.imageSecretsPath, config.CSV.imageSecretsFilename);
 
 esClient._updateESWithImageSecrets(csvPath);
-
-
