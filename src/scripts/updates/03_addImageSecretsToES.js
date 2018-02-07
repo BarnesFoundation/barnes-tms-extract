f75ccba624c59f6cbd4ff1f3196c52517f7b99b8
Jonathan Dahan <hi@jonathan.is>
2018-02-07 13:27:59 -0500

    Revert "Clean up image secrets script."
    This reverts commit 11b7419be876eac2259851d3f758f2d66b83c2c5.

--- src/scripts/updates/03_addImageSecretsToES.js
+++ src/scripts/updates/03_addImageSecretsToES.js
@@ -1,9 +1,16 @@
 const config = require('config');
 const path = require('path');
-const ESCollection = require('../../csv_es/src/script/esCollection.js');
-const { makeElasticsearchOptions } = require('../../util/elasticOptions.js');
+const fs = require('fs');
+const csv = require('fast-csv');
+const eachSeries = require('async/eachSeries');
 
-const esClient = new ESCollection(makeElasticsearchOptions(), config.CSV.imageSecretsPath);
+const { csvForEach } = require('../../util/csvUtil.js');
+const ESCollection = require('../../csv_es/src/script/esCollection.js');
+
+const csvDir = config.CSV.imageSecretsPath;
+const { makeElasticsearchOptions } = require('../../util/elasticOptions.js');
+const esClient = new ESCollection(makeElasticsearchOptions(), csvDir);
+
 const csvPath = path.join(config.CSV.imageSecretsPath, config.CSV.imageSecretsFilename);
 
 esClient._updateESWithImageSecrets(csvPath);
