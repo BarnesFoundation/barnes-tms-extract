fdc67505c0760e010a005295bbdf1ab3c306da27
Jonathan Dahan <hi@jonathan.is>
2018-02-14 12:15:24 -0500

    Use images already uploaded to s3

--- src/scripts/updates/03_processAndUploadImagesToS3.js
+++ src/scripts/updates/03_processAndUploadImagesToS3.js
@@ -7,11 +7,11 @@ const { makeElasticsearchOptions } = require('../../util/elasticOptions.js')
 
 const csvRootDirectory = require('config').CSV.rootDirectory
 
-const imageUploader = new ImageUploader(csvRootDirectory)
-imageUploader.init().then(() => imageUploader.process())
+//const imageUploader = new ImageUploader(csvRootDirectory)
+//imageUploader.init().then(() => imageUploader.process())
 
-const rawUploader = new RawUploader(csvRootDirectory)
-rawUploader.init().then(() => rawUploader.process())
+//const rawUploader = new RawUploader(csvRootDirectory)
+//rawUploader.init().then(() => rawUploader.process())
 
 const tileUploader = new TileUploader(csvRootDirectory)
 tileUploader.init().then(() => tileUploader.process())
