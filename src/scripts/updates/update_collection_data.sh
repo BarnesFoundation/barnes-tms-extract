ba02cb7d34215e90fc5f9c68a3eb73ad2ccff81a
Jonathan Dahan <hi@jonathan.is>
2018-04-23 15:06:40 -0400

    work from any directory for update script

--- src/scripts/updates/update_collection_data.sh
+++ src/scripts/updates/update_collection_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 shopt -s nullglob
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for script in [0-9][0-9]_*.js; do
+for script in ${DIR}/[0-9][0-9]_*.js; do
   node $script
 done
