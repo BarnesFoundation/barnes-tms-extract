e8ffcd59366434091cf29c93f12d5827ec255482
Jonathan Dahan <jonathan@micahwalter.studios>
2018-06-04 20:05:20 +0000

    formatting, comment out tiling

--- src/image-processing/src/script/tmsImageFetch.js
+++ src/image-processing/src/script/tmsImageFetch.js
@@ -10,18 +10,20 @@ const parseTMSImageURL = require('./parseTMSImageURL.js');
 function fetchAvailableImages() {
 	logger.info('Starting fetch available images.');
 	const outputPath = path.resolve(__dirname, '../../names.json');
+	//const outputPath = path.resolve(__dirname, '../../names.justone.json');
+        //return Promise.resolve(outputPath)
 	const { imageUrl, username, password } = config.Credentials.tms;
 	let tmsImageUrl = url.parse(imageUrl)
 	tmsImageUrl.auth= `${username}:${password}`
 	return parseTMSImageURL(tmsImageUrl)
-    .then(res => {
-      fs.writeFileSync(outputPath, JSON.stringify(res));
-      return Promise.resolve(outputPath);
-    })
-    .catch(e => {
-      logger.error(e);
-      process.exit(-1);
-    })
+	    .then(res => {
+	      fs.writeFileSync(outputPath, JSON.stringify(res));
+	      return Promise.resolve(outputPath);
+	    })
+	    .catch(e => {
+	      logger.error(e);
+	      process.exit(-1);
+	    })
 }
 
 module.exports = fetchAvailableImages;
--- src/scripts/updates/04_resizeAndTileImages.js
+++ src/scripts/updates/04_resizeAndTileImages.js
@@ -6,8 +6,9 @@ const { makeElasticsearchOptions } = require('../../util/elasticOptions.js')
 const csvRootDirectory = require('config').CSV.rootDirectory
 
 // Does this make new tiles?
-const tileUploader = new TileUploader(csvRootDirectory)
-tileUploader.init().then(() => tileUploader.process())
+// FIXME: this takes days to complete...
+//const tileUploader = new TileUploader(csvRootDirectory)
+//tileUploader.init().then(() => tileUploader.process())
 
 const imageResizerAndUploader = new ImageResizerAndUploader(csvRootDirectory, makeElasticsearchOptions())
 imageResizerAndUploader.init().then(() => imageResizerAndUploader.process())
