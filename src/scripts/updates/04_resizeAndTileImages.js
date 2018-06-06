833aaf6fb08d567303387f77e21d990a99d0bcc5
Jonathan Dahan <jonathan@micahwalter.studios>
2018-06-06 19:26:19 +0000

    re-enable tiling

--- src/scripts/updates/04_resizeAndTileImages.js
+++ src/scripts/updates/04_resizeAndTileImages.js
@@ -5,10 +5,8 @@ const { makeElasticsearchOptions } = require('../../util/elasticOptions.js')
 
 const csvRootDirectory = require('config').CSV.rootDirectory
 
-// Does this make new tiles?
-// FIXME: this takes days to complete...
-//const tileUploader = new TileUploader(csvRootDirectory)
-//tileUploader.init().then(() => tileUploader.process())
+const tileUploader = new TileUploader(csvRootDirectory)
+tileUploader.init().then(() => tileUploader.process())
 
 const imageResizerAndUploader = new ImageResizerAndUploader(csvRootDirectory, makeElasticsearchOptions())
 imageResizerAndUploader.init().then(() => imageResizerAndUploader.process())
